5679d4b7e204225e291d8c9e324abda88c701c7d
ltt1501 <104372253+ltt1501@users.noreply.github.com>
2022-04-26 22:45:07 +0700

    Update intro.js

--- Intro/intro.js
+++ Intro/intro.js
@@ -1,1 +1,1 @@
-var tik=setInterval(()=>{var t=document.getElementById("digit");t.classList.toggle("digit"),0==t.innerText?(document.getElementById("container").classList.toggle("containeroverlay"),/*window.location="./MyBirthday/MyBirthday.html"*/,clearInterval(tik)):--t.innerText,t.classList.toggle("digit")},1e3);
+var tik=setInterval(()=>{var t=document.getElementById("digit");t.classList.toggle("digit"),0==t.innerText?(document.getElementById("container").classList.toggle("containeroverlay"),clearInterval(tik)):--t.innerText,t.classList.toggle("digit")},1e3);
